2ff5db24301a70e5ed477d8cab5c0d83837e671b
Lee Zamparo <zamparo@gmail.com>
2025-02-16 22:09:54 -0500

    done this kata

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,40 +1,5 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -42,5 +7,102 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class GildedRose(object):
+    def __init__(self, items):
+        self.items = items
+
+    def clone_items(self) -> list[Item]:
+        cloned_items = []
+        for item in self.items:
+            cloned_items.append(Item(item.name, item.sell_in, item.quality))
+        return cloned_items
+
+    def update_quality_passes(self, item):
+        # passes logic here
+        if item.sell_in < 0:
+            item.quality = 0
+
+        if item.sell_in > 10:
+            if item.quality < 50:
+                item.quality += 1
+
+        elif 5 <= item.sell_in < 11:
+            if item.quality < 49:
+                item.quality += 2
+
+        elif 0 <= item.sell_in < 5:
+            if item.quality < 48:
+                item.quality += 3
+
+    def update_sell_in(self, item):
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+
+    def update_legendary(self, item):
+        item.sell_in = item.sell_in
+        item.quality = item.quality
+
+    def update_item_quality(self, item):
+        """Update item quality according to rules"""
+
+        if item.name.startswith("Backstage passes"):
+            self.update_quality_passes(item)
+
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            self.update_legendary(item)
+
+        elif item.name == "Aged Brie":
+            if item.quality < 50:
+                item.quality += 1
+
+        elif item.name.startswith("Conjured"):
+            if item.sell_in > 0:
+                item.quality -= 2
+            else:
+                item.quality -= 4
+
+        else:
+            adjustment = -1 if item.sell_in > 0 else -2
+            item.quality += adjustment
+
+    def update_quality(self):
+        """Update sell-in, quality according to rules"""
+        for item in self.items:
+            self.update_sell_in(item)
+            self.update_item_quality(item)
+
+    # def update_quality(self):
+    #     for item in self.items:
+    #         if (
+    #             item.name != "Aged Brie"
+    #             and item.name != "Backstage passes to a TAFKAL80ETC concert"
+    #         ):
+    #             if item.quality > 0:
+    #                 if item.name != "Sulfuras, Hand of Ragnaros":
+    #                     item.quality = item.quality - 1
+    #         else:
+    #             if item.quality < 50:
+    #                 item.quality = item.quality + 1
+    #                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
+    #                     if item.sell_in < 11:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #                     if item.sell_in < 6:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+
+    #         if item.name != "Sulfuras, Hand of Ragnaros":
+    #             item.sell_in = item.sell_in - 1
+
+    #         if item.sell_in < 0:
+    #             if item.name != "Aged Brie":
+    #                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
+    #                     if item.quality > 0:
+    #                         if item.name != "Sulfuras, Hand of Ragnaros":
+    #                             item.quality = item.quality - 1
+    #                 else:
+    #                     item.quality = item.quality - item.quality
+    #             else:
+    #                 if item.quality < 50:
+    #                     item.quality = item.quality + 1
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,131 @@
 # -*- coding: utf-8 -*-
-import unittest
-
+import pytest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+# fixtures go here
+@pytest.fixture
+def items():
+    items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
+    return items
 
-        
-if __name__ == '__main__':
-    unittest.main()
+
+@pytest.fixture
+def gilded_rose_foo():
+    items = [Item("foo", 0, 0)]
+    return GildedRose(items)
+
+
+@pytest.fixture
+def gilded_rose_items(items):
+    return GildedRose(items)
+
+
+@pytest.fixture
+def gilded_rose_backstage_passes(request):
+    items = [
+        Item(
+            name="Backstage passes to a TAFKAL80ETC concert",
+            sell_in=request.param,
+            quality=25,
+        )
+    ]
+    return GildedRose(items)
+
+
+# tests below here
+def test_foo(gilded_rose_foo):
+    gilded_rose_foo.update_quality()
+    assert "foo" == gilded_rose_foo.items[0].name
+
+
+def test_items_quality_negative(gilded_rose_items):
+    gilded_rose_items.update_quality()
+    for item in gilded_rose_items.items:
+        assert item.quality >= 0
+
+
+def test_items_quality_max(gilded_rose_items):
+    gilded_rose_items.update_quality()
+    for item in gilded_rose_items.items:
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            assert item.quality <= 50
+
+
+def test_items_quality_degrade(gilded_rose_items):
+    # have to clone the items to compare
+    # before and after update_quality
+    previous_items = gilded_rose_items.clone_items()
+    gilded_rose_items.update_quality()
+    for old_item, item in zip(previous_items, gilded_rose_items.items):
+        if item.name not in (
+            "Sulfuras, Hand of Ragnaros",
+            "Aged Brie",
+            "Backstage passes to a TAFKAL80ETC concert",
+        ):
+            assert old_item.name == item.name
+            assert old_item.quality > item.quality, print(
+                f"item: {item.name}, old q: {old_item.quality}, new q: {item.quality}"
+            )
+
+
+def test_items_quality_aged_bree(gilded_rose_items):
+    previous_items = gilded_rose_items.clone_items()
+    gilded_rose_items.update_quality()
+    for old_item, item in zip(previous_items, gilded_rose_items.items):
+        if item.name == "Aged Brie":
+            assert item.quality > old_item.quality
+
+
+def test_items_quality_sulfuras(gilded_rose_items):
+    previous_items = gilded_rose_items.clone_items()
+    gilded_rose_items.update_quality()
+    for old_item, item in zip(previous_items, gilded_rose_items.items):
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            assert old_item.name == item.name
+            assert old_item.quality == item.quality
+
+
+@pytest.mark.parametrize("gilded_rose_backstage_passes", ([10]), indirect=True)
+def test_items_quality_backstage_passes_rate(gilded_rose_backstage_passes):
+    previous_items = gilded_rose_backstage_passes.clone_items()
+    gilded_rose_backstage_passes.update_quality()
+    for old_item, item in zip(previous_items, gilded_rose_backstage_passes.items):
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            assert item.quality == old_item.quality + 2
+
+
+@pytest.mark.parametrize("gilded_rose_backstage_passes", ([5]), indirect=True)
+def test_items_quality_backstage_passes_fastrate(gilded_rose_backstage_passes):
+    previous_items = gilded_rose_backstage_passes.clone_items()
+    gilded_rose_backstage_passes.update_quality()
+    for old_item, item in zip(previous_items, gilded_rose_backstage_passes.items):
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            assert item.quality == old_item.quality + 3
+
+
+@pytest.mark.parametrize("gilded_rose_backstage_passes", ([0]), indirect=True)
+def test_items_quality_backstage_passes_date(gilded_rose_backstage_passes):
+    gilded_rose_backstage_passes.update_quality()
+    for item in gilded_rose_backstage_passes.items:
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            assert item.quality == 0
+
+
+def test_items_quality_conjured(gilded_rose_items):
+    previous_items = gilded_rose_items.clone_items()
+    gilded_rose_items.update_quality()
+    for old_item, item in zip(previous_items, gilded_rose_items.items):
+        if item.name.startswith("Conjured"):
+            assert item.name == old_item.name
+            assert item.quality == old_item.quality - 2
